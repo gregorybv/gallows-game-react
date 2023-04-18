19d37b519ff4e5cc1d7ee7c96520c89b7e771fb3
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-18 08:38:20 +0300

    new width

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -105,7 +105,7 @@ export function HangmanDrawing() {
       <div
         style={{
           height: "400px",
-          width: "10px",
+          width: "15px",
           background: "black",
           marginLeft: "120px",
         }}
